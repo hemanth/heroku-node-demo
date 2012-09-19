162eb6270f968fbe018595cbe7efea5b65305959
hemanth.hm <hemanth.hm@gmail.com>
2012-09-19 12:20:45 +0530

    Changed echo port

--- echo.js
+++ echo.js
@@ -1,6 +1,5 @@
-#!/usr/bin/env node
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+  , wss = new WebSocketServer({port: 1337});
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
